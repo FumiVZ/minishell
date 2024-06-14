c459339269ef58a3bfe5217028475d40ad4abcfe
Vincent Zuccarello <vzuccare@student.42lyon.fr>
2024-06-14 15:09:30 +0200

    fix: resolve memory leaks in logical operators

--- code/exec/child.c
+++ code/exec/child.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   child.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: machrist <machrist@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: vzuccare <vzuccare@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 15:53:07 by vzuccare          #+#    #+#             */
-/*   Updated: 2024/06/14 00:54:13 by machrist         ###   ########.fr       */
+/*   Updated: 2024/06/14 15:08:53 by vzuccare         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -132,5 +132,9 @@ int	child_crt(t_pipex *pipex, char **env)
 			(!(ft_strncmp(pipex->cmd[pipex->i], "&&", 2) == 0)
 				|| !(ft_strncmp(pipex->cmd[pipex->i], "||", 2) == 0)))
 			pipex->i++;
+	free_l(pipex->cmds);
+	pipex->cmds = NULL;
+	free(pipex->pid);
+	pipex->pid = NULL;
 	return (pipex->i);
 }
